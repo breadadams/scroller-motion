211ed9f251c5c2dd7226bf3b7c458a39110d5f32
Brad Adams <hi@breadadams.com>
2022-01-31 20:56:20 +0100

    chore: dependency updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
 module.exports = {
   addons: ['@storybook/addon-links/register', 'storybook-addon-jsx'],
-  stories: ['../stories/**/*.stories.tsx']
+  stories: ['../stories/**/*.stories.tsx'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+
+    return config
+  }
 }
