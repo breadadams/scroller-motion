1c7c8ce843454ac14448378c163422dc070e014b
Brad Adams <hi@breadadams.com>
2022-03-27 15:18:34 +0200

    refactor: internal `useSize` hook

--- src/hooks/useCore/useSize.ts
+++ src/hooks/useCore/useSize.ts
@@ -1,47 +1,41 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import debounceFn from 'debounce-fn'
 
 import { ChildrenRef } from '../../types'
 
-const getSize = (el: HTMLElement) => ({
+const getSize = (el: HTMLElement | null) => ({
   height: el?.scrollHeight ?? 0,
   width: el?.scrollWidth ?? 0
 })
 
+const supportsResizeObserver = typeof ResizeObserver === 'function'
+
 export const useSize = (ref: ChildrenRef) => {
   const [size, setSize] = useState({ height: 0, width: 0 })
 
-  const onResize = useCallback(() => {
-    if (ref.current) {
-      setSize(getSize(ref.current))
-    }
-  }, [])
-
   useEffect(() => {
+    const onResize = () => setSize(getSize(ref.current))
+    const debouncedOnResize = debounceFn(onResize, { wait: 400 })
+    let resizeObserver: ResizeObserver | null = null
+
     if (ref.current) {
       onResize()
-
-      const supportsResizeObserver = typeof ResizeObserver === 'function'
-      let resizeObserver: ResizeObserver | null = null
-
-      const debouncedOnResize = debounceFn(onResize, { wait: 400 })
-
-      window.addEventListener('resize', debouncedOnResize, false)
+      window.addEventListener('resize', debouncedOnResize)
 
       if (supportsResizeObserver) {
         resizeObserver = new ResizeObserver(onResize)
         resizeObserver.observe(ref.current)
       }
+    }
 
-      return () => {
-        window.removeEventListener('resize', debouncedOnResize, false)
+    return () => {
+      window.removeEventListener('resize', debouncedOnResize)
 
-        if (supportsResizeObserver && resizeObserver) {
-          resizeObserver.disconnect()
-        }
+      if (resizeObserver) {
+        resizeObserver.disconnect()
       }
     }
-  }, [onResize])
+  }, [])
 
   return size
 }
