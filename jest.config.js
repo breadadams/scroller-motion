e7fa31131e02dedd7f5d41afe8f679c1c70deebb
Brad Adams <hi@breadadams.com>
2022-08-04 07:17:41 +0200

    tests: include code coverage
    + Update jest config
    + Update GH test action

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 const { defaults } = require('jest-config')
 
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+
   testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, './dist']
 }
